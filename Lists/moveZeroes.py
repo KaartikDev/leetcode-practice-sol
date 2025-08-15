297bce7d771bd402ffda48ae8bbde0ee154645f9
Kaartik Tejwani <tejwanikaartik@gmail.com>
2025-08-15 16:34:54 -0700

    Revised Sol 238 v2

--- Lists/moveZeroes.py
+++ Lists/moveZeroes.py
@@ -5,13 +5,13 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
 
-        check_index = 0
+        nonzero_index = 0
         for i in range(len(nums)):
             if nums[i] != 0:
                 temp = nums[i]
-                nums[i] = nums[check_index]
-                nums[check_index] = temp
-                check_index+=1
+                nums[i] = nums[nonzero_index]
+                nums[nonzero_index] = temp
+                nonzero_index+=1
         """
         Time complexity of O(n), iterate through entire arr
         Space complexity O(1)
